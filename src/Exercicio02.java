61b088ffa0d20e50979ce2f6ba43aa9b573ca34b
Irlan <irlansh@hotmail.com>
2021-06-15 23:54:46 -0300

    Criação do Exercicio02

---
+++ src/Exercicio02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		/*Sabendo que cada hora dura 60 minutos e que cada minuto dura 60 segundos, 
+		 * solicite ao usuário que digite o tempo de um filme em minutos e 
+		 * informe a duração desse filme em horas e em segundos.
+		 */
+		Scanner leitor = new Scanner(System.in);
+		int tempo,hora,minutos;
+		System.out.println("Digite a duração do filme em minutos");
+		tempo = leitor.nextInt();
+		
+		hora = tempo / 60;
+		minutos = tempo % 60;
+		System.out.println("O tempo de duração do filme em horas e minutos são : Horas " + hora + " Minutos " + minutos);
+		
+		leitor.close();
+	}
+
+}
